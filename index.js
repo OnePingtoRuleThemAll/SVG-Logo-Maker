99eb683f5d61351969c353345cf0e235317e4293
Mona Lisa <Sissy1310@aol.com>
2023-06-14 18:09:00 -0500

    update on files

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const filesystem = require() //add path to file system
-const inquirer = require("inquirer");
-const {Circle, Square, Triangle} = require("./lib/shapes");
+const fs = require('fs'); //add path to file system
+const inquirer = require('inquirer');
+const path = require('path');
+const {generateSVG, makeShape} = require('./lib/makeSvg');
+
+inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "text",
+        message: "Text: Enter up to (3) characters:",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "Text Color: Enter a color keyword:",
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "Shape Color: Ender a color keyword:",
+    },
+    {
+        type: "list",
+        name: "pixel-image",
+        message: "Choose which Pixel Image you would like?",
+        choices: {"Circle", "Square", "Triangle"},
+    }
+]);
